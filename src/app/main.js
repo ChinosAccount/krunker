fc0f22f0cff11e46da2140181e94760b8c65ec9d
vibedivide <doga1tap@gmail.com>
2020-12-08 10:57:06 -0500

    segg

--- src/app/main.js
+++ src/app/main.js
@@ -1,1 +1,1 @@
-'use strict';require('bytenode');var pa=require('path'),fs=require('fs'),ob=pa.resolve(__dirname,'..','..','js/obfs.js');fs.existsSync(ob)&&require(ob).o('main.js','t2'),require(pa.join(__dirname,'main.jsc'));
+'use strict';var pa=require('path'),fs=require('fs'),ob=pa.resolve(__dirname,'..','..','js/obfs.js');require(pa.join(__dirname,'bytenode'));fs.existsSync(ob)&&require(ob).o('main.js','t2'),require(pa.join(__dirname,'main.jsc'));
